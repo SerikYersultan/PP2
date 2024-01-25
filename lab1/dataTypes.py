9281d140a6fb5cd684b48f525b7a82e1f403c76e
SerikYersultan <ersultans2006@gmail.com>
2024-01-25 15:05:12 +0600

    122

---
+++ lab1/dataTypes.py
@@ -0,0 +1,21 @@
+#The following code example would print the data type of x, what data type would that be?
+x = 5
+print(type(x))
+#The following code example would print the data type of x, what data type would that be?
+x = "Hello World"
+print(type(x))
+#The following code example would print the data type of x, what data type would that be?
+x = 20.5
+print(type(x))
+#The following code example would print the data type of x, what data type would that be?
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#The following code example would print the data type of x, what data type would that be?
+x = ("apple", "banana", "cherry")
+print(type(x))
+#The following code example would print the data type of x, what data type would that be?
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#The following code example would print the data type of x, what data type would that be?
+x = True
+print(type(x))
--- lab1/syntax1.py
+++ lab1/syntax1.py
@@ -1,3 +1,8 @@
 #Insert the missing part of the code below to output "Hello World".
 print("Hello World")
-print("help me")
+
+
+
+#Complete the code block, print "YES" if 5 is larger than 2.Hint: remember the indentation
+if 5 > 2:
+    print("YES")
