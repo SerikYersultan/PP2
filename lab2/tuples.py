d8188edfce2005e1e2afff97a6084ada3d3655f1
SerikYersultan <ersultans2006@gmail.com>
2024-02-01 13:48:25 +0600

    l

--- lab2/tuples.py
+++ lab2/tuples.py
@@ -51,4 +51,64 @@ print(thistuple[-1])
 #Возвращает третий, четвертый и пятый элемент:
 thistuple = ("яблоко", "банан", "вишня", "апельсин", "киви", "дыня", "манго")
 print(это число [2:5])
+#Example
+#This example returns the items from the beginning to, but NOT included, "kiwi":
 
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+#Example
+#This example returns the items from "cherry" and to the end:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+#Example
+#This example returns the items from index -4 (included) to index -1 (excluded)
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+#Example
+#Check if "apple" is present in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+
+#ExampleGet your own Python Server
+#Convert the tuple into a list to be able to change it:
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+#Example
+#Convert the tuple into a list, add "orange", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+
+#Example
+#Create a new tuple with the value "orange", and add that tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+
+#Example
+#Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+
+#
