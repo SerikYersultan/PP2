941e8c84d9ebac6c92de1976db11d0ceedd942bd
SerikYersultan <ersultans2006@gmail.com>
2024-02-01 17:28:45 +0600

    455

---
+++ lab2/forloops/forExercises.py
@@ -0,0 +1,37 @@
+#Exercise:
+#Loop through the items in the fruits list.
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+
+  print(x)
+
+#Exercise:
+#In the loop, when the item value is "banana", jump directly to the next item.
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+
+  print(x)
+
+#Exercise:
+#Use the range function to loop through a code set 6 times.
+
+
+for x in range(6):
+  print(x)
+
+#Exercise:
+#Exit the loop when x is "banana".
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+
+  print(x)
