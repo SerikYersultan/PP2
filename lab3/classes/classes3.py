75894e307e622563652f8ffa13328cf07ebfd846
SerikYersultan <ersultans2006@gmail.com>
2024-02-09 21:19:00 +0600

    uyyt

--- lab3/classes/classes3.py
+++ lab3/classes/classes3.py
@@ -3,7 +3,7 @@ Define a class named Rectangle which inherits from Shape class from task 2.
 Class instance can be constructed by a length and width. 
 The Rectangle class has a method which can compute the area.
 """
-class Rectangle(Shape):
+class Rectangle():
     def __init__(self, length, width):
         super().__init__()
         self.length = length
